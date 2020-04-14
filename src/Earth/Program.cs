8207442e1f412018044a160678730d93ac66b2e2
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-15 02:47:32 +0430

    Projects

---
+++ src/Earth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Earth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Message :");
+            var message = Console.ReadLine();
+        }
+    }
+}
---
+++ src/Moon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Moon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
