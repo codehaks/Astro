a59d9e5b1387e796e1f72ebc964d9e994ceb6d5b
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-15 03:49:36 +0430

    Fix

--- src/Moon/Program.cs
+++ src/Moon/Program.cs
@@ -11,8 +11,24 @@ namespace Moon
         {
             Console.WriteLine("Hello World!");
 
-            var listener = new RabbitListener();
-            listener.Register();
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "chatroom", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(message);
+
+            };
+            channel.BasicConsume(queue: "chatroom", autoAck: false, consumer: consumer);
+
+            Console.ReadLine();
 
 
         }
@@ -34,7 +50,7 @@ namespace Moon
             }
             public void Register()
             {
-                Channel.QueueDeclare(queue: "chatroom", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                
 
                 var consumer = new EventingBasicConsumer(Channel);
                 consumer.Received += (model, ea) =>
