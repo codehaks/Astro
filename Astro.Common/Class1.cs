ffa21a390723eca2028873f49a79a6615f3983e9
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-20 18:45:41 +0430

    Fix

---
+++ Astro.Common/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Astro.Common
+{
+    public class Message
+    {
+        public string From { get; set; }
+        public string Body { get; set; }
+        public DateTime TimeCreated { get; set; }
+    }
+}
---
+++ Mars/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Mars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //--- Setup
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+
+            channel.QueueDeclare(queue: "inbox",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            //--- Chat
+
+            var message = " ";
+
+            while (message != string.Empty)
+            {
+                Console.WriteLine("Message : ");
+                message = Console.ReadLine();
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                     routingKey: "inbox",
+                     basicProperties: null,
+                     body: body);
+
+                Console.WriteLine(" [Earth] Sent {0}", message);
+            }
+        }
+    }
+}
