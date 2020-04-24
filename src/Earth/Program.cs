a8fb51c6b37c5d6b4eee098e562eb15094483532
Hakim Ghods <ghods.hakim@hotmail.com>
2020-04-24 13:27:49 +0430

    fix

--- src/Earth/Program.cs
+++ src/Earth/Program.cs
@@ -16,27 +16,64 @@ namespace Earth
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
-            
 
-            channel.QueueDeclare(queue: "inbox",
+            //direct,fanout,topic,header
+            channel.ExchangeDeclare("contact", "direct", false,false, null);
+            channel.ExchangeDeclare("alarms", "fanout", false, false, null);
+            channel.ExchangeDeclare("subject", "topic", false, false, null);
+
+
+            channel.QueueDeclare(queue: "a",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
+            channel.QueueDeclare(queue: "b",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            channel.QueueDeclare(queue: "c",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            //channel.QueueBind("a", "contact", "blue");
+            //channel.QueueBind("b", "contact", "blue");
+            //channel.QueueBind("c", "contact", "red");
+
+            //channel.QueueBind("a", "contact", "black");
+            //channel.QueueBind("b", "contact", "black");
+            //channel.QueueBind("c", "contact", "black");
+
+            //channel.QueueBind("a", "alarms", "");
+            //channel.QueueBind("b", "alarms", "");
+            //channel.QueueBind("c", "alarms", "");
+
+            channel.QueueBind("a", "subject", "*.orders.*");
+            channel.QueueBind("b", "subject", "high.*");
+            channel.QueueBind("c", "subject", "logging.#");
+
+
             //--- Chat
 
             var message = " ";
-
+            var key = "";
             while (message!=string.Empty)
             {
-                Console.WriteLine("Message : ");
+                Console.Write("Message : ");
                 message = Console.ReadLine();
-     
+
+                Console.Write("Key : ");
+                key = Console.ReadLine();
+
                 var body = Encoding.UTF8.GetBytes(message);
 
-                channel.BasicPublish(exchange: "",
-                     routingKey: "inbox",
+                channel.BasicPublish(exchange: "subject",
+                     routingKey: key,
                      basicProperties: null,
                      body: body);
 
