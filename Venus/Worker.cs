954ddfdfc0cfd44fef958fffc06f927df543b3bd
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-04 00:58:17 +0430

    Fix

---
+++ Venus/Worker.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Venus
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "inbox", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(queue: "inbox", autoAck: true, consumer: consumer);
+
+            await Task.CompletedTask;
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        }
+    }
+}
--- src/Earth/Program.cs
+++ src/Earth/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Newtonsoft.Json;
 using RabbitMQ.Client;
 using System;
-using System.Collections.Generic;
 using System.Text;
 
 namespace Earth
@@ -18,72 +17,27 @@ namespace Earth
             using var connection = factory.CreateConnection();
             using var channel = connection.CreateModel();
 
-            //direct,fanout,topic,header
-            channel.ExchangeDeclare("contact", "direct", false, false, null);
-            channel.ExchangeDeclare("alarms", "fanout", false, false, null);
-            channel.ExchangeDeclare("subject", "topic", false, false, null);
-            channel.ExchangeDeclare("format", "headers", false, false, null);
 
-
-            channel.QueueDeclare(queue: "a",
+            channel.QueueDeclare(queue: "inbox",
                                  durable: false,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
 
-            channel.QueueDeclare(queue: "b",
-                                durable: false,
-                                exclusive: false,
-                                autoDelete: false,
-                                arguments: null);
-
-            channel.QueueDeclare(queue: "c",
-                                durable: false,
-                                exclusive: false,
-                                autoDelete: false,
-                                arguments: null);
-
-            var headerBind1 = new Dictionary<string, object>();
-            headerBind1.Add("x-match", "any");
-            headerBind1.Add("format", "bmp");
-            channel.QueueBind("a", "format", "", headerBind1);
-
-
-            var headerBind2 = new Dictionary<string, object>();
-            headerBind2.Add("x-match", "any");
-            headerBind2.Add("format", "mkv");
-            channel.QueueBind("b", "format", "", headerBind2);
-
-            channel.QueueBind("c", "format", "");
-
-
             //--- Chat
 
             var message = " ";
-            var format = "";
+
             while (message != string.Empty)
             {
-                Console.Write("Message : ");
+                Console.WriteLine("Message : ");
                 message = Console.ReadLine();
-
-                Console.Write("Format : ");
-                format = Console.ReadLine();
-
+                string msg = JsonConvert.SerializeObject(message);
                 var body = Encoding.UTF8.GetBytes(message);
 
-                var properties = channel.CreateBasicProperties();
-                var headers = new Dictionary<string, object>
-                {
-                    { "format", format },
-                    { "time", DateTime.Now.ToShortTimeString()
-                    }
-                };
-
-                properties.Headers = headers;
-
-                channel.BasicPublish(exchange: "format",
-                     routingKey: format,
-                     basicProperties: properties,
+                channel.BasicPublish(exchange: "",
+                     routingKey: "inbox",
+                     basicProperties: null,
                      body: body);
 
                 Console.WriteLine(" [Earth] Sent {0}", message);
@@ -94,4 +48,3 @@ namespace Earth
         }
     }
 }
-
